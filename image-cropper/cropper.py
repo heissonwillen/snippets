d4d611879be99fcf358bec39e7777d6ca95f44c9
heisson <heisson70@gmail.com>
2020-06-21 09:33:04 -0300

    Refactoring and removed sensitive files

--- image-cropper/cropper.py
+++ image-cropper/cropper.py
@@ -1,14 +1,14 @@
 from PIL import Image
 
 last_pos_x, last_pos_y = 0, 0
-GRID_SIZE = 10
+GRID_DIMENSION = 10
 
 image = Image.open("image.jpg")
 
-width, height = tuple([int(dimension/GRID_SIZE) for dimension in list(image.size)])
+width, height = tuple([int(dimension/GRID_DIMENSION) for dimension in list(image.size)])
 
-for i in range(GRID_SIZE):
-    for j in range(GRID_SIZE):
+for i in range(GRID_DIMENSION):
+    for j in range(GRID_DIMENSION):
         cropped_image = image.crop((last_pos_x, last_pos_y, last_pos_x+width, last_pos_y+height))
         cropped_image.save(f"cropped_images/image-{i}-{j}.jpg")
 
@@ -16,3 +16,5 @@ for i in range(GRID_SIZE):
 
     last_pos_x = 0
     last_pos_y += height
+
+print('done :)')
