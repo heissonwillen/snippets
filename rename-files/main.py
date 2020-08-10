17e40a38918c040dc77bbb3b772a6e6f5f23452c
heissonwillen <heisson70@gmail.com>
2020-08-10 07:49:39 -0300

    Refactoring rename-files

--- rename-files/main.py
+++ rename-files/main.py
@@ -1,11 +1,7 @@
 import os
 
 for file in os.listdir():
-    file_name, file_extension = os.path.splitext(file)
+    name, extension = os.path.splitext(file)
 
-    if file_extension != '.py':
-        new_name = f'{file_name.split(".")[3][-3:]}{file_extension}'
-
-        # print(new_name)
-
-        os.rename(file, new_name)
+    if extension != '.py':
+        os.rename(file, f'{name.split(".")[3][-3:]}{extension}')
